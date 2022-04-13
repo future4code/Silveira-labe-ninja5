c0c25f10c7b572863a5d04f15681802fcaabedd4
graziela-queiroz <99085346+graziela-queiroz@users.noreply.github.com>
2022-04-12 23:29:44 -0300

    footer-filtro-ordenacao

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FormularioCadastro from'./pages/FormularioCadastro'
 import TelaCarrinho from'./pages/TelaCarrinho'
 import TelaServicos from'./pages/TelaServicos'
 import TelaServicosDetalhe from'./pages/TelaServicosDetalhe'
+import Footer from './components/Footer/Footer'
 
 
 export default class App extends React.Component {
@@ -37,6 +38,9 @@ export default class App extends React.Component {
 			<div>
 				<Header mudarTela={this.mudarTela}/>
 				{this.escolherTela()}
+
+				<Footer mudarTela={this.mudarTela}/>
+
 			</div>
 		)
 	}
--- src/components/Ordenacao/Ordenacao.jsx
+++ src/components/Ordenacao/Ordenacao.jsx
@@ -1,1 +1,41 @@
+import React, { Component } from 'react';
+// import styled from 'styled-components'
 
+
+export default class Ordenacao extends React.Component {
+    state = {
+        sort: "CRESCENTE"
+    }
+      
+    getFilteredList = () => {
+        return this.props.products
+        .filter((obj) => this.props.maxValue === "" || obj.valor <= this.props.maxValue)
+        .filter((obj) => this.props.minValue === "" ||  obj.valor >= this.props.minValue)
+        .filter((obj) => obj.titulo.toLowerCase().includes(this.props.tituloValue.toLowerCase()))
+        .sort((a, b) => 
+            this.state.sort === 'CRESCENTE' ? a.valor - b.valor : 
+            this.state.sort === 'DECRESCENTE' ? b.valor - a.valor : 
+            this.state.sort === 'PRAZO' ? a.prazo - b.prazo : 
+            this.state.sort === 'TITULO' ? a.titulo - b.titulo : null) 
+    }
+
+    onChangeSelect = (event) => this.setState({sort: event.target.value});
+
+    render(){
+        // const {semOrdenacao: valor} = this.props
+        // const filteredList = this.getFilteredList();
+        return(
+            <label className="semOrdenacao">Ordenacao:
+                <select 
+                    value={this.state.sort}
+                    onChange={this.onChangeSelect}>
+                    <option value="valorMax">Valor Max</option>
+                    <option value="valorMin">Valor Min</option>
+                    <option value="titulo">Título</option>
+                    <option value="prazo">Prazo</option>
+                </select>
+
+            </label>
+        )
+    }
+}
